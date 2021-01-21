92605be9b00efd66104092663f69792865015baa
Zbeyde Özge YILMAZ <zozgeyilmaz@hotmail.com>
2021-01-22 02:01:54 +0300

    Added Length method in MyList class

--- GenericsIntro/Program.cs
+++ GenericsIntro/Program.cs
@@ -7,7 +7,11 @@ namespace GenericsIntro
         static void Main(string[] args)
         {
             MyList<string> isimler = new MyList<string>();
-            isimler.Add("Özge");
+            isimler.Add("Engin");
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("Kerem");
+            Console.WriteLine(isimler.Length);
 
             Console.WriteLine("Hello World!");
         }
